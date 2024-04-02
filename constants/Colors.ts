575f2c89777340f63ed1a21e87447f3161353ec7
ana <anabarquero.ab@gmail.com>
2024-04-02 09:02:47 -0600

    💬  add literals for colors and order launch past

--- constants/Colors.ts
+++ constants/Colors.ts
@@ -1,2 +1,19 @@
 export const Colors = {
+  white: '#ffffff',
+  primary: {
+    500: '#193247',
+  },
+  secondary: {
+    500: '#d14b39',
+  },
+  lightyellow: {
+    200: '#fdf2dd',
+    500: '#f4ddb5',
+  },
+  gray: {
+    200: '#606060',
+  },
+  orange: {
+    500: '#d14b39',
+  },
 };
---
+++ constants/LaunchPast.ts
@@ -0,0 +1,8 @@
+import { OrderLaunchesPastObject } from '../types/common';
+
+export const ORDER_LAUNCHES_PAST: OrderLaunchesPastObject = {
+  launch_year: 'Launch Year',
+  rocket_type: 'Rocket Type',
+  rocket_name: 'Rocket Name',
+  mission_name: 'Mission Name',
+};
